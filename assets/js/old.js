d2f5c72837089b018f651b24c1ca8d1b738643e2
Greg Ladden <greg@gl-digital.com>
2017-10-15 01:14:50 -0500

    input add

--- assets/js/old.js
+++ assets/js/old.js
@@ -1,11 +1,17 @@
+// ====================================================
+//                   Gloabl Variables
+// ====================================================
 var selectedWord = "";
-var wordLine = [];
-var theWord = [];
+var lettersInWord = [];
+var letterLine = [];
+var displayText = "";
+
 
 var currentsentence = 0;
 var imageClue = [];
+var quizOver = false;
 
-
+// game object
 
 var question = [{
     sentence: "Prince Ali's real name is A ",
@@ -25,33 +31,62 @@ var question = [{
     imageClue: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-0AliAZEYudZbT1YoblX3bgXveNfFfzFM8FkQhAJDzjWp-hAo"
 
 }];
- 
+
+
+
+
 // ====================================================
 //                   Functions
 // ====================================================
 
 function displayCurrentsentence() {
     
-    $("#usr").val("");
     $(".prizeBG").hide();
-    $('.nextBtn').hide();
 
     var sentence = question[currentsentence].sentence;
     var sentenceClass = $(document).find(".sentence");
     var wordToGuess = question[currentsentence].word.length;
 
+    // resetting letter line
+    letterLine = [];
     // // Remove all current <li> elements (if any)
     $(".sentence").find("p").remove();
 
     
     for (i = 0; i < wordToGuess; i++) {
         selectedWord = question[currentsentence].word[i];
-        theWord = selectedWord;
-        console.log(theWord, "the word");
+        lettersInWord = selectedWord.split("");
+        numBlanks = lettersInWord.length;
     }
-    
-    $(".sentence").html(sentence);
 
+    for (var i = 0; i < numBlanks; i++) {
+        letterLine.push("_");
+    }
+
+    $(".sentence").html(sentence);
+    $(".guess").html(letterLine.join("  "));
+
+}
+
+function checkLetters(letter) {
+    // Check if letter exists in Code
+    var isLetterInWord = false;
+
+    for (var i = 0; i < numBlanks; i++) {
+        if(lettersInWord[i] == letter) {
+            isLetterInWord = true;
+        }
+    }
+
+    // Check where in word letter exists and then populate out blankAndSuccesses array
+    if (isLetterInWord) {
+        for (var i = 0; i < numBlanks; i++) {
+
+            if (lettersInWord[i] == letter) {
+                letterLine[i] = letter;
+            }
+        }
+    }    
 }
 
 function imgGif() {
@@ -59,10 +94,19 @@ function imgGif() {
 }
 
 function roundComplete() {
+
+    $(".guess").html(letterLine.join("  "));
+
+    if (lettersInWord.toString() == letterLine.toString()) {
+        // displaying word guessed once guessed correctly
+        displayText += " " +  letterLine.toString().replace(/,/g, "");
+
+        $(".prizeAnswer").html('<p class=" answerText center-text">' + displayText + '</p>');
+    
         imgGif();
         $(".imageClue").hide();
         $('.theGif').show();
-    
+    }
 }
 
 function thePrize() {
@@ -75,6 +119,8 @@ function imagesClue() {
     
     console.log('imgclue', imgClue);
     $(".imageClue").html('<img src="'+imgClue+'">');
+    
+
 }
 
 function music () {
@@ -99,71 +145,127 @@ function music () {
 // ====================================================
 //                   Main Process
 // ====================================================
+$(document).ready(function () {
+    // music();
+    displayCurrentsentence();
+    imagesClue();
 
-displayCurrentsentence();
-imagesClue();
+    // Register Key clicks
+    document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 
-$(function() {
-    $("form").submit(function() { return false; });
-});
-
-// On clicking next, display the next sentence
-$(document).find("#userInput").on("click", function() {
-
-    var goodGuess = $('#usr').val();
-
-    if (theWord != goodGuess) {
-        alert("You need to complete the sentence before moving on.");
-    
-        } 
-        else 
-        {
-            if (theWord == goodGuess){
-
-                roundComplete();
-                displayText = " " +  goodGuess;
-                $(".prizeAnswer").append('<p class=" answerText center-text">'  + displayText + " " + '</p>');
-                $('.nextBtn').show();
-                $('#game').hide();
-                $('.sentence').hide();
-
-            }
-        }
-});
-
-// On clicking next, display the next sentence
-$(document).find(".nextBtn").on("click", function() {
-    
-    var goodGuess = $('#usr').val();
-
-    if (theWord != goodGuess) {
-        alert("You need to complete the sentence before moving on.");
-    
-        } 
-        else 
-        {
-        currentsentence++; // Since we have already displayed the first sentence on DOM ready
+    if (lettersInWord.toString() != letterLine.toString()) {
         
-        if (currentsentence < question.length) {
-            displayCurrentsentence();
-            imagesClue();
-            $(".imageClue").show();
-            $('.theGif').hide();
-            $('.nextBtn').hide();
-            $('#game').show();
-            $('.sentence').show();
-
-        }  
-        else {
-            $(".sentence, .guess, .questionAnswer, .nextBtn, .jumbotron").hide();
-            $(".theGif").hide();
-            $(".imageClue").hide();
-            $(".prizeAnswer").hide();
-            $(".clue").hide();
-            thePrize();
+        checkLetters(letterGuessed);
+        roundComplete();
+    
         }
     }
-});
-    
 
+    // On clicking next, display the next sentence
+    $(document).find(".nextBtn").on("click", function() {
+    ;
+        if (lettersInWord.toString() != letterLine.toString()) {
+            alert("You need to complete the sentence before moving on.");
+        
+            } else {
+                currentsentence++; // Since we have already displayed the first sentence on DOM ready
+                
+                if (currentsentence < question.length) {
+                    displayCurrentsentence();
+                    imagesClue();
+                    roundComplete();
+                    $(".imageClue").show();
+                    $('.theGif').hide();
+                }  else {
+                    $(".sentence, .guess, .questionAnswer, .nextBtn, .jumbotron").hide();
+                    $(".theGif").hide();
+                    $(".imageClue").hide();
+                    $(".prizeAnswer").hide();
+                    $(".clue").hide();
+                    thePrize();
+                }
+            }
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function startGame() {
+
+//     for (var i = 0; i < question.length; i++){
+//         var sentences = question[i].sentence;
+//         var words = question[i].word;
+//         console.log(sentences);
+//         console.log(words);
     
+//         selectedWord = words[Math.floor(Math.random() * words.length)];
+//         lettersInWord = selectedWord.split("");
+//         numBlanks = lettersInWord.length;
+//         console.log("numblanks", numBlanks)
+
+//     }
+
+//      // Populate Letter Line with right number of blanks
+//     for (var i = 0; i < numBlanks; i++) {
+//         letterLine.push("_");
+//     }
+
+//     numBlanks = letterLine.join(" ");
+
+//     // Adding sentence + word to document 
+//     var gameContainer = $('<div>')
+//     var gameSentence = $("<p>").text(sentences + " " + numBlanks);
+
+//     gameContainer.append(gameSentence);
+
+//     $("#sentence").append(gameContainer);
+   
+
+//     // Test //
+//     console.log(lettersInWord);
+        
+// }
+
+// function checkLetters(letter) {
+//     // Check if letter exists in Code
+//     var isLetterInWord = false;
+
+//     for (var i = 0; i < numBlanks; i++) {
+//         if(selectedWord[i] == letter) {
+//             isLetterInWord = true;
+            
+//         }
+//     }
+
+//     // Check where in word letter exists and then populate out blankAndSuccesses array
+//     if (isLetterInWord) {
+//         for (var i = 0; i < numBlanks; i++) {
+//             if (selectedWord[i] == letter) {
+//                 letterLine[i] = letter;
+//             }
+//         }
+//     } // if didn't find letter
+//      else {
+//          wrongLetters.push(letter);
+//          guessLeft--;
+//      }
+// }
+
+// // ====================================================
+// //                   Main process
+// // ====================================================
+
+// startGame();
+// checkLetters();
